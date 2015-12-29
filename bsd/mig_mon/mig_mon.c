79bcf5feae9087c21cd1ddbb62c53c19a5d54bc1
Peter Xu <peterx@redhat.com>
2015-12-29 12:08:46 +0800

    mig_mon: add spike_log feature
    Now, we could specify the server with a spike log parameter:
    
    This file will store all the spikes during measurement. The spikes are
    defined as:
    
      spike_throttle = first_latency * 2
    
    Signed-off-by: Peter Xu <peterx@redhat.com>

--- bsd/mig_mon/mig_mon.c
+++ bsd/mig_mon/mig_mon.c
@@ -3,10 +3,13 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <stdint.h>
 #include <time.h>
+#include <fcntl.h>
 
 /* whether allow client change its IP */
 #define  MIG_MON_SINGLE_CLIENT       (0)
@@ -18,11 +21,22 @@ static const char *prog_name = NULL;
 
 void usage(void)
 {
-    printf("usage: %s server\n", prog_name);
+    printf("usage: %s server [spike_log]\n", prog_name);
     printf("       %s client server_ip [interval_ms]\n", prog_name);
     puts("");
     puts("This is a program that could be used to measure");
     puts("VM migration down time. Please specify work mode.");
+    puts("");
+    puts("Example usage to measure guest server downtime:");
+    puts("");
+    printf("1. [on guest]  start server using '%s server /tmp/spike.log'\n",
+           prog_name);
+    printf("   this will start server, log all spikes into spike.log.\n");
+    printf("2. [on client] start client using '%s client GUEST_IP 50'\n",
+           prog_name);
+    printf("   this starts sending UDP packets to server, interval 50ms.\n");
+    printf("3. trigger loop migration (e.g., 100 times)\n");
+    printf("4. see the results on server side.\n");
 }
 
 uint64_t get_msec(void)
@@ -40,7 +54,33 @@ uint64_t get_msec(void)
     return val;
 }
 
-int mon_server(void)
+uint64_t get_timestamp(void)
+{
+    return (uint64_t)time(NULL);
+}
+
+void write_spike_log(int fd, uint64_t delay)
+{
+    char spike_buf[1024] = {0};
+    int str_len = -1;
+    str_len = snprintf(spike_buf, sizeof(spike_buf) - 1,
+                       "%ld,%ld\n", get_timestamp(), delay);
+    spike_buf[sizeof(spike_buf) - 1] = 0x00;
+    write(fd, spike_buf, str_len);
+    /* not flushed to make it fast */
+}
+
+/*
+ * spike_log is the file path to store spikes. Spikes will be
+ * stored in the form like (for each line):
+ *
+ * A,B
+ *
+ * Here, A is the timestamp in seconds. B is the latency value in
+ * ms. If it is NULL, then no log is written at all (which is the
+ * default behavior).
+ */
+int mon_server(const char *spike_log)
 {
     char buf[BUF_LEN];
     int sock = 0;
@@ -49,6 +89,25 @@ int mon_server(void)
     socklen_t addr_len = sizeof(clnt_addr);
     in_addr_t target = -1;
     uint64_t last = 0, cur = 0, delay = 0, max_delay = 0;
+    int spike_fd = -1;
+    /*
+     * this will store the 1st and 2nd UDP packet latency, as a
+     * baseline of latency values (this is very, very possibly the
+     * value that you provided as interval when you start the
+     * client). This is used to define spikes, using formular:
+     *
+     *         spike_throttle = first_latency * 2
+     */
+    uint64_t first_latency = -1, spike_throttle = -1;
+
+    if (spike_log) {
+        spike_fd = open(spike_log, O_WRONLY | O_CREAT, 0644);
+        if (spike_fd == -1) {
+            perror("failed to open spike log");
+            return -1;
+        }
+        ftruncate(spike_fd, 0);
+    }
 
     bzero(&svr_addr, sizeof(svr_addr));
     bzero(&clnt_addr, sizeof(clnt_addr));
@@ -114,6 +173,27 @@ int mon_server(void)
 
         /* this is the packet we want to measure with */
         delay = cur - last;
+
+        /* if this is _exactly_ the 2nd packet we got, we need to
+         * note this down as a baseline. */
+        if (first_latency == -1) {
+            first_latency = delay;
+            printf("1st and 2nd packet latency: %lu (ms)\n", first_latency);
+            spike_throttle = delay * 2;
+            printf("Setting spike throttle to: %lu (ms)\n", spike_throttle);
+            if (spike_fd != -1) {
+                printf("Updating spike log initial timestamp\n");
+                /* this -1 is meaningless, shows the init timestamp only. */
+                write_spike_log(spike_fd, -1);
+            }
+        }
+
+        /* if we specified spike_log, we need to log spikes into
+         * that file. */
+        if (spike_fd != -1 && delay >= spike_throttle) {
+            write_spike_log(spike_fd, delay);
+        }
+
         /* update LAST */
         last = cur;
         if (delay > max_delay)
@@ -183,6 +263,7 @@ int main(int argc, char *argv[])
     int interval_ms = MIG_MON_INT_DEF;
     const char *work_mode = NULL;
     const char *server_ip = NULL;
+    const char *spike_log = NULL;
 
     prog_name = argv[0];
 
@@ -194,7 +275,10 @@ int main(int argc, char *argv[])
     work_mode = argv[1];
     if (!strcmp(work_mode, "server")) {
         puts("starting server mode...");
-        ret = mon_server();
+        if (argc >= 3) {
+            spike_log = argv[2];
+        }
+        ret = mon_server(spike_log);
     } else if (!strcmp(work_mode, "client")) {
         if (argc < 3) {
             usage();
