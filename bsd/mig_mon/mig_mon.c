6486bb231011867d41ab8c9e3e198112e131fcc1
Peter Xu <peterx@redhat.com>
2021-11-17 15:26:41 +0800

    mig_mon: v0.1.2
    Signed-off-by: Peter Xu <peterx@redhat.com>

--- bsd/mig_mon/mig_mon.c
+++ bsd/mig_mon/mig_mon.c
@@ -23,7 +23,7 @@ typedef enum {
     PATTERN_NUM,
 } dirty_pattern;
 
-#define  VERSION  "v0.1.1"
+#define  VERSION  "v0.1.2"
 
 char *pattern_str[PATTERN_NUM] = { "sequential", "random", "once" };
 
